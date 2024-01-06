695c72d0a29b72a6a2c33559cadd093b6e35b410
Prakhar <prakharsomu014@gmail.com>
2024-01-06 12:31:07 +0530

    Added react-icons

--- src/components/TodoDisplay.jsx
+++ src/components/TodoDisplay.jsx
@@ -1,6 +1,7 @@
 import { useContext, useRef} from "react";
 import styles from './TodoItem.module.css';
 import { TodoContext } from "./store/todoContext";
+import { IoMdAddCircleOutline } from "react-icons/io";
 const TodoDisplay=()=>{
 
   const todoNameRef = useRef();
@@ -19,7 +20,7 @@ const TodoDisplay=()=>{
     <div className={styles.input}>
     <input type='text' placeholder='Enter Todo Here' className='text' ref={todoNameRef} />
     <input type='Date' className='date' ref={todoDateRef}/>
-    <button className="btn btn-success">Add </button>
+    <button className="btn btn-success"><IoMdAddCircleOutline/></button>
   </div>  
   </form>
   )
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import styles from './TodoItems.module.css';
 import { TodoContext } from './store/todoContext';
+import { MdDelete } from "react-icons/md";
 const TodoItems=({name,date})=>{
   const {deleteItem} = useContext(TodoContext);
 return (
@@ -9,7 +10,7 @@ return (
         <li className={styles.todos}>
         <span>{name}</span>
         <span>{date}</span>
-        <button type="button" className="btn btn-danger" onClick={()=>deleteItem(name)}>Delete</button>
+        <button type="button" className="btn btn-danger" onClick={()=>deleteItem(name)}><MdDelete/></button>
         </li>
   </>
 )
